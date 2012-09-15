38efc6d09fd0dafdd6afe0af52036c3430ae1ca5
Ashu Goel <soxfan981@gmail.com>
2012-09-14 23:23:55 -0400

    redid

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ElectioneeringRails::Application.config.secret_token = 'a4eb0725e3dbe87af244889c9e2d2dd68544b3d16d1060227a6c03a59c9cf98944cf91bb5149f10b8535b9b26d1a9096af807ba899252550429e59832cd9c4c0'
