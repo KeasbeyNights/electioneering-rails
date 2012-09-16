d66e67aa212a325a81ba28990e01e626fe821356
guenint <tedguenin@gmail.com>
2012-09-16 02:39:27 -0400

    colorize v1

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -5,4 +5,19 @@ class Issue
   key :stance, String
   
   belongs_to :politician
+
+  def colorize(that)
+    if self.name.eql?('Abortion') || self.name.eql?('Education') || 
+      self.name.eql?('Energy and the Environment') || 
+      self.name.eql?('Gay Marriage') || self.name.eql?('Health Care') ||
+      self.name.eql?('Immigration')
+      if self.stance.eql?(that.stance)
+        "green"
+      else
+        "red"
+      end
+    else
+      "blue"
+    end
+  end
 end
